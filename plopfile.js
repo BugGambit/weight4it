80d4a2c394e2e6e239f9e48319c88c6ebf3320ef
Fredrik Anfinsen <fredrik.anfinsen@gmail.com>
2020-12-27 15:28:07 +0100

    refactor: added plop for code generation (eg. add new component)

---
+++ plopfile.js
@@ -0,0 +1,20 @@
+module.exports = function (plop) {
+  // create your generators here
+  plop.setGenerator('component', {
+    description: 'react component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{name}}/{{name}}.tsx',
+        templateFile: 'plop-templates/component.hbs',
+      },
+    ],
+  });
+};
